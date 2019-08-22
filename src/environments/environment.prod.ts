6d3ea81640ffab68b572fa881d1875817a3eb735
Pranav <pranav@newput.com>
2019-08-22 12:50:17 +0530

    monthobj added in prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   baseUrl: 'https://salary-slip-be.herokuapp.com',
   navigateUrl: 'https://salary-slip.herokuapp.com',
   months : ['January', 'February', 'March', 'April', 'May',
-    'June', 'July', 'August', 'September', 'October', 'November', 'December']
-  // navigateUrl: 'hello'
+    'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthObj : { "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6,
+    "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12}
 };
